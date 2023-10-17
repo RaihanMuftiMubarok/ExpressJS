6d313a0f45bab4d5a3c22fb4c623434f889cc871
RaihanMuftiMubarok <raihanmiqdad203@.com>
2023-10-17 13:46:40 +0700

    tugas16

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -104,7 +104,7 @@ router.get('/(:id)', function (req, res){
     })
 })
 
-router.patch('/update/:id', upload.single("gambar") ,[
+router.patch('/update/:id', upload.fields([{ name: 'gambar', maxCount: 1 }, { name: 'swa_foto', maxCount: 1 }]) ,[
     body('nama').notEmpty(),
     body('nrp').notEmpty(),
     body('id_jurusan').notEmpty(),
@@ -117,7 +117,8 @@ router.patch('/update/:id', upload.single("gambar") ,[
     }
     
     let id = req.params.id;
-    let gambar = req.file ? req.file.filename : null;
+    let gambar = req.files['gambar']? req.files['gambar'][0].filename : null;
+    let swa_foto = req.files['swa_foto']? req.files['swa_foto'][0].filename : null;
     connection.query(`select * from mahasiswa where id_m = ${id}`, function (err, rows){
         if(err){
             return res.status(500).json({
@@ -130,18 +131,24 @@ router.patch('/update/:id', upload.single("gambar") ,[
                 message: 'Not Found',
             })
         }
-        const namaFileLama = rows[0].gambar;
+        const gambarLama = rows[0].gambar;
+        const swa_fotoLama = rows[0].swa_foto;
     
         //hapus file lama
-        if (namaFileLama && gambar){
-            const pathFileLama = path.join(__dirname,'../public/images', namaFileLama);
-            fs.unlinkSync(pathFileLama);
+        if (gambarLama && gambar){
+            const pathGambar = path.join(__dirname,'../public/images', gambarLama);
+            fs.unlinkSync(pathGambar);
+        }
+        if (swa_fotoLama && swa_foto){
+            const pathSwaFoto = path.join(__dirname,'../public/images', swa_fotoLama);
+            fs.unlinkSync(pathSwaFoto);
         }
         let Data = {
             nama: req.body.nama,
             nrp: req.body.nrp,
             id_jurusan: req.body.id_jurusan,
             gambar: gambar,
+            swa_foto: swa_foto,
         }
         connection.query(`update mahasiswa set ? where id_m = ${id}`, Data, function (err, rows){
             if(err){
